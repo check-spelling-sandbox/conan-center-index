ebb9ae903af0baa81387c231293cd5c9ba7bd3d3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-30 21:22:49 -0500

    spelling: headers
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- recipes/djinni-support-lib/1.x.x/test_package/test_package.cpp
+++ recipes/djinni-support-lib/1.x.x/test_package/test_package.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 
 // since we would either need objective C++ or Java (jni) there is not a lot to test
-// just that we have the hearders and can link
+// just that we have the headers and can link
 int main() {
     return EXIT_SUCCESS ;
 }
