10c865af7890ce20ae135f5b20dea14fd7f071db
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-01 00:32:27 -0500

    spelling: until
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- recipes/boost/all/test_package/locale.cpp
+++ recipes/boost/all/test_package/locale.cpp
@@ -30,7 +30,7 @@ int main()
     // Display current year
     std::cout << format("{1,ftime='%Y'}") % now << std::endl;
     //
-    // Run forward untill current year is the date
+    // Run forward until current year is the date
     //
     for(now=start; period::year(now) == current_year;) {
         // Print heading of month
