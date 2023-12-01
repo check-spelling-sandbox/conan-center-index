2e5f75b16ab9951f9932926c361f9e938a797c0f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-01 00:16:17 -0500

    spelling: theorem
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- recipes/units/all/test_package/test_package.cpp
+++ recipes/units/all/test_package/test_package.cpp
@@ -10,6 +10,6 @@ int main()
 {
   meter_t a = 3_m;
   meter_t b = 4_m;
-  meter_t c = sqrt(pow<2>(a) + pow<2>(b)); // Pythagorean threorem.
+  meter_t c = sqrt(pow<2>(a) + pow<2>(b)); // Pythagorean theorem.
   std::cout << c << std::endl; // prints: "5 m"
 }
