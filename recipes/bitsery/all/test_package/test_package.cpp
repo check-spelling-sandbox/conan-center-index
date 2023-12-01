01a40259488951dce8bb149de1a8856d41d029fd
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-01 00:32:08 -0500

    spelling: unnecessary
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- recipes/bitsery/all/test_package/test_package.cpp
+++ recipes/bitsery/all/test_package/test_package.cpp
@@ -3,7 +3,7 @@
 #include <bitsery/bitsery.h>
 //in ordered to serialize/deserialize data to buffer, include buffer adapter
 #include <bitsery/adapter/buffer.h>
-//bitsery itself doesn't is lightweight, and doesn't include any unnessessary files,
+//bitsery itself doesn't is lightweight, and doesn't include any unnecessary files,
 //traits helps library to know how to use types correctly,
 //in this case we'll be using vector both, to serialize/deserialize data and to store use as a buffer.
 #include <bitsery/traits/vector.h>
